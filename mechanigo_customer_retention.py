b532c57064f4086c9d9c15c33ef610f87eed5edf
LICA Data Team <datateam.licagroup@gmail.com>
2023-06-05 17:31:25 +0800

    Add files via upload

--- mechanigo_customer_retention.py
+++ mechanigo_customer_retention.py
@@ -990,27 +990,27 @@ if __name__ == '__main__':
                 """)
                 
         # filters
-        with st.expander('Customer Data Filters'):
-            st.subheader('Last Transaction Date')
-            min_txn, max_txn = st.columns(2)
-            with min_txn:
-                min_txn_date = st.date_input('Min Transaction Date:',
-                          min_value = df_data.appointment_date.min(),
-                          max_value = datetime.today(),
-                          value = df_data.appointment_date.min())
-            with max_txn:
-                max_txn_date = st.date_input('Max Transaction Date:',
-                          min_value = df_data.appointment_date.min(),
-                          max_value = datetime.today(),
-                          value = datetime.today())
+        # with st.expander('Customer Data Filters'):
+        #     st.subheader('Last Transaction Date')
+        #     min_txn, max_txn = st.columns(2)
+        #     with min_txn:
+        #         min_txn_date = st.date_input('Min Transaction Date:',
+        #                   min_value = df_data.appointment_date.min(),
+        #                   max_value = datetime.today(),
+        #                   value = df_data.appointment_date.min())
+        #     with max_txn:
+        #         max_txn_date = st.date_input('Max Transaction Date:',
+        #                   min_value = df_data.appointment_date.min(),
+        #                   max_value = datetime.today(),
+        #                   value = datetime.today())
                 
-            df_temp.loc[:, 'last_txn_date'] = pd.to_datetime(df_temp.last_txn_date)
-            df_retention = df_temp[(df_temp.last_txn_date.dt.date >= min_txn_date) & (df_temp.last_txn_date.dt.date <= max_txn_date)]
-            df_retention.loc[:, 'last_txn_date'] = df_retention.last_txn_date.apply(lambda x: x.strftime('%Y/%m/%d'))
-            df_temp.loc[:, 'last_txn_date'] = df_temp.last_txn_date.apply(lambda x: x.strftime('%Y/%m/%d'))
+        #     df_temp.loc[:, 'last_txn_date'] = pd.to_datetime(df_temp.last_txn_date)
+        #     df_retention = df_temp[(df_temp.last_txn_date.dt.date >= min_txn_date) & (df_temp.last_txn_date.dt.date <= max_txn_date)]
+        #     df_retention.loc[:, 'last_txn_date'] = df_retention.last_txn_date.apply(lambda x: x.strftime('%Y/%m/%d'))
+        #     df_temp.loc[:, 'last_txn_date'] = df_temp.last_txn_date.apply(lambda x: x.strftime('%Y/%m/%d'))
             
         time = 30
-        df_retention = update_retention(pnbd, ggf, time, df_retention)
+        df_retention = update_retention(pnbd, ggf, time, df_temp)
          
         customer_retention_list = customer_search(df_data, df_retention)
         
